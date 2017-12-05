13a315389b012e811d22f62a9ab7c6bd14e7c7bb
Timothy <trediehs@g.ucla.edu>
2017-12-04 18:55:24 -0800

    Nothing Changed, Just Added Everything Back

---
+++ AstroSolver/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class frmMain
+
+    Private Sub btnNewtonSynth_Click(sender As Object, e As EventArgs) Handles btnNewtonSynth.Click
+        frmNewtonSynthesis.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub btnStefanBoltzmann_Click(sender As Object, e As EventArgs) Handles btnStefanBoltzmann.Click
+        frmStefanBoltzmann.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub btnDopShift_Click(sender As Object, e As EventArgs) Handles btnDopShift.Click
+        frmHubblesLaw.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub btnDistMod_Click(sender As Object, e As EventArgs) Handles btnDistMod.Click
+        FrmDistanceModulus.Show()
+        Me.Close()
+    End Sub
+End Class
